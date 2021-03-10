1d91fc06f81b2a88a54e9c2c8dda5234e8bdc9ea
Mark Frigon <mark@angel.pics>
2021-03-09 17:09:08 -0700

    Add project files.

---
+++ SignalR/SignalR.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using System.Threading.Tasks;
+
+namespace SignalR
+{
+    public static class Functions
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "chat")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        [FunctionName("messages")]
+        public static async Task SendMessage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] object message,
+            [SignalR(HubName = "chat")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            await signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "newMessage",
+                    Arguments = new[] { message }
+                });
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>	
+	public partial class MainWindow : Window
+	{
+		private HubConnection hub = null;
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				hub = new HubConnectionBuilder()
+					.WithUrl("http://localhost:7071/api")
+					.Build();
+				hub.On<string,string>("newMessage", (result, one) => {
+					MessageBox.Show(result);
+				});
+
+				await hub.StartAsync();
+			}catch(Exception ex)
+			{
+
+			}
+		}
+	}
+}
